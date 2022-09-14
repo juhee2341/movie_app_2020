2128ffaaad7d65d703a4888cbc00671f631954e8
이주희(누리플렉스) <jhlee96@nuriflex.co.kr>
2022-09-14 14:33:37 +0900

    react Upload

---
+++ src/components/Movie.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropsType from 'prop-types';
+import './Movie.css';
+import { Link } from 'react-router-dom';
+
+function Movie({ title, year, summary, poster, genres}) {
+    return (
+        <div className="movie">
+            <Link
+                to={{
+                    pathname: '/movie-detail',
+                    state: { year, title, summary, poster, genres },
+                }}
+            >
+            <img src={poster} alt={title} title={title}/>
+            <div className="movie_data">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <ul className="movie__genres">
+                    {genres.map((genres, index) => {
+                        return <li key={index} className="movie__genre">{genres}</li>
+                    })}
+                </ul>
+                <p className="movie__summary">{summary.slice(0, 180)}</p>
+            </div>
+            </Link>
+        </div>
+    );
+}
+
+Movie.prototype = {
+    year: PropsType.number.isRequired,
+    title: PropsType.number.isRequired,
+    summary: PropsType.number.isRequired,
+    poster: PropsType.number.isRequired,
+    genres: PropsType.arrayOf(PropsType.string).isRequired,
+};
+
+export default Movie;
